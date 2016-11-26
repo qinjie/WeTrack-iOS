cefdb0dcf0eeb3c32da7907065c15c137b7a697f
xuhelios <xuhelios@Trans-Mac-mini.local>
2016-11-26 22:37:06 +0800

    a

---
+++ Documents/xu/README.md
@@ -0,0 +1,2 @@
+# xuhelios
+hiihh
---
+++ Documents/xu/example/UserLoginAndRegisterExampleInSwift-master/UserLoginAndRegistration/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.swift
+//  UserLoginAndRegistration
+//
+//  Created by Sergey Kargopolov on 2015-01-13.
+//  Copyright (c) 2015 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginButtonTapped(_ sender: AnyObject) {
+        
+        let userEmail = userEmailTextField.text;
+        let userPassword = userPasswordTextField.text;
+        
+        let userEmailStored = UserDefaults.standard.string(forKey: "userEmail");
+        
+        let userPasswordStored = UserDefaults.standard.string(forKey: "userPassword");
+        
+        if(userEmailStored == userEmail)
+        {
+            if(userPasswordStored == userPassword)
+            {
+                // Login is successfull
+                UserDefaults.standard.set(true,forKey:"isUserLoggedIn");
+                    UserDefaults.standard.synchronize();
+                
+               self.dismiss(animated: true, completion:nil);
+            }
+        }
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
